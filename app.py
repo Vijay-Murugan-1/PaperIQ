6582685b8cdc3872bc6a098ab1f95b5b5bb6b152
Vijay-Murugan-1 <aspiringdataeng@gmail.com>
2026-06-16 23:20:06 +0530

    Added app.py and tested basic Streamlite functions

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+def main():
+    st.title("PaperIQ!")
+    st.subheader("AI-based research assistant for papers.")
+    st.write("PaperIQ is an AI-powered research assistant designed to help you quickly understand and analyze academic papers. Simply upload a paper, and PaperIQ will provide you with a concise summary, key insights, and answers to your questions about the content. Whether you're a student, researcher, or just curious, PaperIQ makes it easier than ever to stay informed and up-to-date with the latest research.")
+if __name__ == "__main__":    main()    
+
+
+    
