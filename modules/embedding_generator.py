32939c284f19da9c6d7b106aa92369157bb8ea22
Vijay-Murugan-1 <aspiringdataeng@gmail.com>
2026-07-04 23:14:44 +0530

    Implement semantic embedding generation module.

--- modules/embedding_generator.py
+++ modules/embedding_generator.py
@@ -1,17 +1,57 @@
+"""
+Embedding generation utilites for PaperIQ
+
+This module provides helper function to provide vector 
+embeddings for the research paper chunks using 
+Sentence Transformers.These embeddings are later used for
+semnatic retrieval in the RAG pipeline.
+"""
+
 import streamlit as st
+import numpy as np
 from sentence_transformers import SentenceTransformer
 
 @st.cache_resource
 def load_embedding_model():
+    """
+    Load and Cache the Sentence Transformer model.
+
+    The model is loaded only once during the application's lifetime
+    and reused for the next eembedding requests.
+
+    Returns:
+        SentenceTransformer: Initialized sentence embedding model.
+    """
     return SentenceTransformer("BAAI/bge-small-en-v1.5")
 
 embedding_model = load_embedding_model()
 
-def generate_embedding(chunk):
+def generate_embedding(chunk: str) -> np.ndarray:
+    """
+    Generate a semantic embedding for a single text chunk.
+
+    Args:
+        chunk (str): Input text chunk that is to be converted
+        to vector represntation.
+
+    Returns:
+        An numpy array representing semantic embeddings.
+
+    """
     embedding = embedding_model.encode(chunk, convert_to_numpy = True)
     return embedding
 
-def generate_embeddings(chunks):
+def generate_embeddings(chunks: list[str]) -> list[np.ndarray]:
+    """
+    Generates semantic embeddings for multiple text chunks.
+
+    Args:
+        chunks(list[str]): List of text chunks extracted 
+        form the document.
+
+    Returns:
+        List containing one embedding vector for each input chunk.
+    """
     embeddings = []
 
     for chunk in chunks:
